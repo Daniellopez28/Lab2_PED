cbe3b6de2ef25dfe2e61f481aace66b686acace7
Daniellopez28 <00156625@uca.edu.sv>
2026-01-16 21:04:45 -0600

    Atributos y metodos

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+void ImprimirLista()
+    {
+        cout << "--- Estudiantes ---" << endl;
+        nodo *temp = inicio;
+        while (temp)
+        {
+            temp->est->imprimir();
+            temp = temp->sig;
+        }
+    }
+
+    float calcularPromedioRecursivo()
+    {
+        int total = 0;
+        float suma = sumarNotas(inicio, total);
+        return total ? suma / total : 0;
+    }
+};
+
+int main()
+{
+    ListaEnlazada lista;
+
+    lista.agregarEstudiante("Diana", 86.5f);
+    lista.agregarEstudiante("Andres", 95.0f);
+    lista.agregarEstudiante("Daniel", 68.5f);
+    lista.agregarEstudiante("Samuel", 88.0f);
+
+    lista.ImprimirLista();
+
+    cout << "Promedio de todas las notas: " << lista.calcularPromedioRecursivo() << endl;
+
+    return 0;
+};

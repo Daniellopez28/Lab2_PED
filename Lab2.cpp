21b8e39ed545045af42bdc2eb11cc213d5823fb2
IsraelHS07 <00200524@uca.edu.sv>
2026-01-16 19:47:49 -0600

    Actualizar clase estudiante

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,3 +1,29 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Estudiantes
+{
+private:
+    string nombre;
+    float Notafinal;
+
+public:
+    Estudiantes(string n, float notaf) : nombre(n), Notafinal(notaf) {}
+    float nota()
+    {
+        return Notafinal;
+    }
+    void imprimir()
+    {
+        cout << nombre << " su nota es: " << Notafinal << endl;
+    }
+};
+
+struct nodo
+{
+    Estudiantes *est;
+    nodo *sig;
+
+    nodo(Estudiantes *e) : est(e), sig(nullptr) {}
+};
